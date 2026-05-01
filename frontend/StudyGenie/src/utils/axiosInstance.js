ceb63b96c966926f54e783a6c597c1c6cb34d7c1
kshiitij3 <kshitijshendkar3@gmail.com>
2026-05-01 18:19:35 +0530

    api paths , services and axiosinstance for backend and frontend integration

--- frontend/StudyGenie/src/utils/axiosInstance.js
+++ frontend/StudyGenie/src/utils/axiosInstance.js
@@ -1,18 +1,44 @@
-import axios from 'axios';
-import { BASE_URL } from './apiPaths';
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  timeout : 10000,
+  timeout: 80000,
   headers: {
-    "content-type":"appication/json",
-     "accept":"application/json",
+    "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
-//request interceptor
+
+// Request Interceptor
 axiosInstance.interceptors.request.use(
-  (config)=>{
-    const accessToken = localStorage.getItem('accessToken');
-    
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-)
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again.");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
