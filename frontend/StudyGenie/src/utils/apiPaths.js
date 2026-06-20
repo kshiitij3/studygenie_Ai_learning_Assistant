84ded83c4bfbbcb9d80d774b304f270d8f38a3e3
kshiitij3 <kshitijshendkar3@gmail.com>
2026-06-20 23:28:36 +0530

    changes for frontend deplyment

--- frontend/StudyGenie/src/utils/apiPaths.js
+++ frontend/StudyGenie/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = 
+    "https://studygenie-ai-learning-assistant-backend.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
